666fe754ca6f73f1b3b34901a8de214d415f9181
bbherrero <herrero.o.b@gmail.com>
2025-10-22 11:38:21 +0200

    Feat: update index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,41 +5,70 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 const requestNotificationPermission = async () => {
-  // Verify if browser supports notifications
   if (!('Notification' in window)) {
     if (process.env.DEBUGGING) console.log('Notifications not supported.')
     return
   }
 
-  const promise = Notification.requestPermission()
-  await promise.then((permission) => {
-    if (permission === 'granted') {
-      if (process.env.DEBUGGING) console.log('Notification permission granted.')
-      new Notification('QUASAR', { body: 'This is a test notification.' })
-    } else {
-      if (process.env.DEBUGGING) console.log('Notification permission denied.')
-    }
-  })
-};
+  const permission = await Notification.requestPermission()
+  if (permission === 'granted') {
+    if (process.env.DEBUGGING) console.log('Notification permission granted.')
+    new Notification('QUASAR', { body: 'This is a test notification.' })
+  } else {
+    if (process.env.DEBUGGING) console.log('Notification permission denied.')
+  }
+}
 
 onMounted(async () => {
   await requestNotificationPermission()
-  // TODO: request here data needed for the app before entering
 
-  router.push('/events')
-
+  setTimeout(() => {
+    router.push('/points')
+  }, 1000)
 })
+
+// const goToPoints = () => {
+//   router.push('/points')
+// }
 </script>
 
 <template>
-  <q-page class="column flex flex-center bg-gradient">
-    <img alt="App logo" src="~assets/quasar-logo-vertical.svg" style="width: 120px; height: auto" />
-    <h1 class="text-h5 text-center q-mt-md"> App Name </h1>
-  </q-page>
+  <div class="background">
+    <q-img src="~assets/images/background.png" img-class="background__img">
+      <div class="background__content">
+        <img alt="App logo" src="~assets/SABE_logo_blanco.png" style="width: auto; height: 75px"
+          class="background__logo" />
+        <!-- <q-btn class="q-mt-xl" color="primary" label="Comenzar" @click="goToPoints" /> -->
+      </div>
+    </q-img>
+  </div>
 </template>
 
-<style lang="css" scoped>
-.bg-gradient {
-  background: linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, rgba(17, 131, 255, 0.15) 50.84%, rgba(17, 131, 255, 0.25) 101.68%);
+<style lang="scss" scoped>
+.q-img {
+  position: inherit;
+}
+
+.background {
+  width: 100vw;
+  height: 100vh;
+  z-index: 10;
+
+  &__img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: none;
+    position: relative;
+    height: 100%;
+  }
+
 }
 </style>
